54e630bf44c5a596e4ce6d95d78e61bf7c5a00c1
liubo20 <liubo20@staff.weibo.com>
2019-07-24 23:05:37 +0800

    update

---
+++ core/src/main/java/com/argyranthemum/common/core/serializer/ResourceListSerializer.java
@@ -0,0 +1,33 @@
+package com.argyranthemum.common.core.serializer;
+
+import com.argyranthemum.common.core.constant.ConfigurationConst;
+import com.argyranthemum.common.core.constant.SystemConst;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ResourceListSerializer extends JsonSerializer<String> {
+    @Override
+    public void serialize(String values, JsonGenerator jgen, SerializerProvider serializers) throws IOException {
+        List<String> urls = Lists.newArrayList();
+        String[] strings = values.split(",");
+        for (String value : strings) {
+            Matcher matcher = Pattern.compile(SystemConst.Pattern.URL_REGULAR_EXPRESSION).matcher(value);
+            if (matcher.matches()) {
+                urls.add(value);
+            } else {
+                value = StringUtils.startsWith(value, "/") ? value : "/" + value;
+                jgen.writeString(ConfigurationConst.RESOURCE_ROOT_URL + value);
+                urls.add(ConfigurationConst.RESOURCE_ROOT_URL + value);
+            }
+        }
+        jgen.writeString(JacksonUtil.write(urls));
+    }
+}
