12943c06f3bff3b27222a9c1320ddcba4d42e863
liubo20 <liubo20@staff.weibo.com>
2019-07-28 22:23:35 +0800

    update

--- core/src/main/java/com/argyranthemum/common/core/serializer/ResourceUrlSerializer.java
+++ core/src/main/java/com/argyranthemum/common/core/serializer/ResourceUrlSerializer.java
@@ -5,22 +5,30 @@ import com.argyranthemum.common.core.constant.SystemConst;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ResourceUrlSerializer extends JsonSerializer<String> {
 
     @Override
-    public void serialize(String value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
-        Matcher matcher = Pattern.compile(SystemConst.Pattern.URL_REGULAR_EXPRESSION).matcher(value);
-        if (matcher.matches()) {
-            jgen.writeString(value);
-        } else {
-            value = StringUtils.startsWith(value, "/") ? value : "/" + value;
-            jgen.writeString(ConfigurationConst.RESOURCE_ROOT_URL + value);
+    public void serialize(String values, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+        List<String> urls = Lists.newArrayList();
+        String[] strings = values.split(",");
+        for (String value : strings) {
+            Matcher matcher = Pattern.compile(SystemConst.Pattern.URL_REGULAR_EXPRESSION).matcher(value);
+            if (matcher.matches()) {
+                urls.add(value);
+            } else {
+                value = StringUtils.startsWith(value, "/") ? value : "/" + value;
+                urls.add(ConfigurationConst.RESOURCE_ROOT_URL + value);
+            }
         }
+        jgen.writeString(Joiner.on(",").join(urls));
     }
 }
