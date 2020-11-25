93faf3dee778f350fac9991030497ba0ba327305
liubo20 <liubo20@staff.weibo.com>
2020-11-25 15:13:16 +0800

    update

--- mybatis/src/main/java/com/argyranthemum/common/mybatis/domain/PagerSupport.java
+++ mybatis/src/main/java/com/argyranthemum/common/mybatis/domain/PagerSupport.java
@@ -15,19 +15,20 @@ import java.util.List;
  * @Author: estn.zuo
  * @CreateTime: 2017-05-13 19:44
  */
-public class PagerSupport {
+public class PagerSupport<S, T> {
 
-    public static Pager merge(IPage page, Class clazz) {
-        List list = BeanUtil.mergeList(page.getRecords(), clazz);
+    public <S, T> PagerSupport() {
+    }
+
+    public Pager<T> merge(IPage<S> page, Class<T> clazz) {
+        List<T> list = BeanUtil.mergeList(page.getRecords(), clazz);
         return copy(page, list);
     }
 
-    public static Pager copy(IPage page, List list) {
-        Pager pager = new Pager(page.getCurrent(), page.getSize());
+    public Pager<T> copy(IPage<S> page, List<T> list) {
+        Pager<T> pager = new Pager<>(page.getCurrent(), page.getSize());
         pager.setTotal(page.getTotal());
         pager.setRecords(list);
         return pager;
     }
-
-
 }
